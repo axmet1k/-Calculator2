f80e4dc32c423604cbebf3bd46d25349773d7c0f
Axmet1K <azaliya.akhmetzhan@mail.ru>
2026-09-20 16:43:12 +0700

    feat: добавлено окно авторизации
    Создан класс MainWindow. Добавлены поля ввода логина и пароля, кнопка «Авторизоваться». Раскладка собрана с помощью Layout.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,7 +1,38 @@
 #include "mainwindow.h"
+#include <QFormLayout>
+#include <QHBoxLayout>
+#include <QMessageBox>
+#include <QVBoxLayout>
+#include <QWidget>
+
+#define n 40
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
-{}
+{
+    this->setWindowTitle("Авторизация в калькулятор");
+    this->resize(600, 200);
+
+    QWidget *central = new QWidget(this);
+    this->setCentralWidget(central);
+
+    login = new QLineEdit(this);
+    login->setPlaceholderText("Введите логин");
+    password = new QLineEdit(this);
+    password->setPlaceholderText("Введите пароль");
+    password->setEchoMode(QLineEdit::Password);
+
+    logIn = new QPushButton("Авторизоваться", this);
+    logIn->setMinimumHeight(50);
+    QFormLayout *formLayout = new QFormLayout;
+    formLayout->addRow(tr("&Login:"), login);
+    formLayout->addRow(tr("&Password:"), password);
+
+    QVBoxLayout *mainLayout = new QVBoxLayout(central);
+    mainLayout->setContentsMargins(n, n, n, n);
+    mainLayout->setSpacing(25);
+    mainLayout->addLayout(formLayout);
+    mainLayout->addWidget(logIn, 0, Qt::AlignVCenter);
+}
 
 MainWindow::~MainWindow() {}
--- mainwindow.h
+++ mainwindow.h
@@ -1,6 +1,10 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include <QLineEdit>
+#include <QMainWindow>
+#include <QPushButton>
+
 //==== Класс главного окна ====
 class MainWindow : public QMainWindow
 {
@@ -9,5 +13,12 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr); // конструктор
     ~MainWindow();                         //дестркуртор
+private slots:
+    // void logInClicked();
+
+private:
+    QLineEdit *login;
+    QLineEdit *password;
+    QPushButton *logIn;
 };
 #endif // MAINWINDOW_H
