9f0c9c00c8783e9a1e8147e117fa018216c94c50
Axmet1K <azaliya.akhmetzhan@mail.ru>
2026-09-22 00:11:56 +0700

    feat: добавлены виджеты на окно меню
    Добавлены кнопки для перехода к мини-программам: Кот Пушин, Выбор, Калькулятор, Игра-стрелялка. Настроен вертикальный layout.

--- menuwindow.cpp
+++ menuwindow.cpp
@@ -1,8 +1,33 @@
 #include "menuwindow.h"
+//#include <QFormLayout>
+//#include <QHBoxLayout>
+#include <QMessageBox>
+#include <QVBoxLayout>
+#include <QWidget>
 
 MenuWindow::MenuWindow(QWidget *parent)
     : QWidget(parent)
 {
     this->setWindowTitle("Меню");
     this->resize(600, 200);
+
+    label1 = new QLabel(this);
+    label1->setText("<h2>Выберите мини-программу</h2>");
+
+    catRadioButton = new QPushButton("Кот Пушин", this);
+    catRadioButton->setMinimumHeight(50);
+    checkBox = new QPushButton("Выбор", this);
+    checkBox->setMinimumHeight(50);
+    calculator = new QPushButton("Калькулятор", this);
+    calculator->setMinimumHeight(50);
+    game = new QPushButton("Игра - стрелярка", this);
+    game->setMinimumHeight(50);
+
+    QVBoxLayout *layout = new QVBoxLayout(this); // layout сразу на this
+    layout->addWidget(label1);
+    layout->addWidget(game);
+    layout->addWidget(calculator);
+    layout->addWidget(catRadioButton);
+    layout->addWidget(checkBox);
 }
+MenuWindow::~MenuWindow() {}
--- menuwindow.h
+++ menuwindow.h
@@ -1,6 +1,8 @@
 #ifndef MENUWINDOW_H
 #define MENUWINDOW_H
 
+#include <QLabel>
+#include <QPushButton>
 #include <QWidget>
 
 class MenuWindow : public QWidget
@@ -8,6 +10,15 @@ class MenuWindow : public QWidget
     Q_OBJECT
 public:
     explicit MenuWindow(QWidget *parent = nullptr);
+    ~MenuWindow();
+private slots:
+
+private:
+    QLabel *label1;
+    QPushButton *catRadioButton;
+    QPushButton *checkBox;
+    QPushButton *calculator;
+    QPushButton *game;
 };
 
 #endif // MENUWINDOW_H
